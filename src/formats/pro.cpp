454e74c53eced52888599640a3964cea36963f56
Adam Kewley <contact@adamkewley.com>
2019-08-08 20:25:55 +0100

    Fixed little bugs

--- src/formats/pro.cpp
+++ src/formats/pro.cpp
@@ -3,6 +3,7 @@
 #include <stdexcept>
 
 using klmth::pro::ObjectType;
+using klmth::pro::FrmType;
 
 const char* klmth::pro::str(ObjectType type) {
   switch (type) {
@@ -20,5 +21,28 @@ const char* klmth::pro::str(ObjectType type) {
     return "misc";
   default:
     throw std::runtime_error{"unknown object type passed into pro::str(ObjectType)"};
+  }
+}
+
+const char* klmth::pro::str(FrmType type) {
+  switch (type) {
+  case FrmType::item:
+    return "item";
+  case FrmType::critter:
+    return "critter";
+  case FrmType::scenery:
+    return "scenery";
+  case FrmType::wall:
+    return "wall";
+  case FrmType::tile:
+    return "tile";
+  case FrmType::background:
+    return "background";
+  case FrmType::interface:
+    return "interface";
+  case FrmType::inventory:
+    return "inventory";
+  default:
+    throw std::runtime_error{"unknown object passed into pro::str(FrmType)"};
   }
 }
