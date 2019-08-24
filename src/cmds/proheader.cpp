cfc780ee7006f88aaa9d06abf7b6195e4cf3e756
Adam Kewley <contact@adamkewley.com>
2019-08-24 13:12:26 +0100

    Got rid over 'using' overuse

--- src/cmds/proheader.cpp
+++ src/cmds/proheader.cpp
@@ -8,12 +8,6 @@
 #include "src/formats/pro_reader.hpp"
 #include "src/utils/cli.hpp"
 
-using std::vector;
-using std::string;
-using std::istream;
-using std::ifstream;
-using std::stringstream;
-using std::ostream;
 using klmth::pro::Header;
 using klmth::pro::ObjectType;
 using klmth::pro::WallData;
@@ -39,18 +33,18 @@ namespace {
   }
 
   template<typename T>
-  void print_kv(ostream& out, const char* k, T v) {
+  void print_kv(std::ostream& out, const char* k, T v) {
     out << k << " = " << v << std::endl;
   }
 
-  void read_and_print_wall(ostream& out, istream& in) {
+  void read_and_print_wall(std::ostream& out, std::istream& in) {
     WallData wd = pro::read_wall_data(in);
     print_kv(out, "wall_orientation", pro::str(wd.orientation));
     print_kv(out, "action_flags", join(", ", pro::flag_strs(wd.action_flags)));
     print_kv(out, "material_id", pro::str(wd.material_id));
   }
 
-  void read_and_print_item(ostream& out, istream& in) {
+  void read_and_print_item(std::ostream& out, std::istream& in) {
     ItemData item = pro::read_item_data(in);
     print_kv(out, "item_type", pro::str(item.type));
     print_kv(out, "material_id", pro::str(item.material_id));
@@ -61,13 +55,13 @@ namespace {
     print_kv(out, "sound_id", item.sound_id);
   }
 
-  void read_and_print_scenery(ostream& out, istream& in) {
+  void read_and_print_scenery(std::ostream& out, std::istream& in) {
     SceneryData scenery = pro::read_scenery_data(in);
     print_kv(out, "orientation", pro::str(scenery.type));
     print_kv(out, "action_flags", join(", ", pro::flag_strs(scenery.action_flags)));
   }
 
-  void print(ostream& out, const Header& h, istream& in) {
+  void print(std::ostream& out, const Header& h, std::istream& in) {
     print_kv(out, "type", pro::str(h.obj_id.type));
     print_kv(out, "object_id", h.obj_id.val);
     print_kv(out, "text_id", h.text_id);
@@ -91,7 +85,7 @@ namespace {
     }
   }
 
-  void run(ostream& out, cli::NamedStream& strm) {
+  void run(std::ostream& out, cli::NamedStream& strm) {
     out << "[" << strm.name << "]" << std::endl;
     Header h = pro::read_header(strm.strm);
     print(out, h, strm.strm);
@@ -100,9 +94,8 @@ namespace {
 }
 
 int cmd_proheader(int argc, char** argv) {
-  vector<string> paths;
-
   CLI::App app{"dump PRO file headers"};
+  std::vector<std::string> paths;
   app.add_option("pro_file", paths, "path to pro file(s). '-' is interpreted as stdin.");
 
   CLI11_PARSE(app, argc, argv);
